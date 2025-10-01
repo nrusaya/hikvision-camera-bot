7fd298324f41b5775a108bd065d6504521c4013e
nrusaya <90027030+nrusaya@users.noreply.github.com>
2025-10-01 03:53:55 +0300

    Update commands.py

--- hikcamerabot/commands.py
+++ hikcamerabot/commands.py
@@ -17,55 +17,55 @@ def setup_commands() -> tuple[dict, dict[list[str] | str, Callable]]:
                 'getvideor_{0}': cb.cmd_getvideor,
             },
         },
-        CmdSectionType.infrared.value: {
-            'commands': {
-                'ir_on_{0}': cb.cmd_ir_on,
-                'ir_off_{0}': cb.cmd_ir_off,
-                'ir_auto_{0}': cb.cmd_ir_auto,
-            },
-        },
-        CmdSectionType.motion_detection.value: {
-            'commands': {
-                'md_on_{0}': cb.cmd_motion_detection_on,
-                'md_off_{0}': cb.cmd_motion_detection_off,
-            },
-        },
-        CmdSectionType.line_detection.value: {
-            'commands': {
-                'ld_on_{0}': cb.cmd_line_detection_on,
-                'ld_off_{0}': cb.cmd_line_detection_off,
-            },
-        },
-        CmdSectionType.intrusion_detection.value: {
-            'commands': {
-                'intr_on_{0}': cb.cmd_intrusion_detection_on,
-                'intr_off_{0}': cb.cmd_intrusion_detection_off,
-            },
-        },
-        CmdSectionType.alert_service.value: {
-            'commands': {
-                'alert_on_{0}': cb.cmd_alert_on,
-                'alert_off_{0}': cb.cmd_alert_off,
-            },
-        },
-        CmdSectionType.stream_youtube.value: {
-            'commands': {
-                'yt_on_{0}': cb.cmd_stream_yt_on,
-                'yt_off_{0}': cb.cmd_stream_yt_off,
-            },
-        },
-        CmdSectionType.stream_telegram.value: {
-            'commands': {
-                'tg_on_{0}': cb.cmd_stream_tg_on,
-                'tg_off_{0}': cb.cmd_stream_tg_off,
-            },
-        },
-        CmdSectionType.stream_icecast.value: {
-            'commands': {
-                'icecast_on_{0}': cb.cmd_stream_icecast_on,
-                'icecast_off_{0}': cb.cmd_stream_icecast_off,
-            },
-        },
+     #   CmdSectionType.infrared.value: {
+     #       'commands': {
+      #          'ir_on_{0}': cb.cmd_ir_on,
+       #         'ir_off_{0}': cb.cmd_ir_off,
+        #        'ir_auto_{0}': cb.cmd_ir_auto,
+         #   },
+       # },
+       # CmdSectionType.motion_detection.value: {
+       #     'commands': {
+       #         'md_on_{0}': cb.cmd_motion_detection_on,
+       #         'md_off_{0}': cb.cmd_motion_detection_off,
+       #     },
+       # },
+       # CmdSectionType.line_detection.value: {
+       #     'commands': {
+        #        'ld_on_{0}': cb.cmd_line_detection_on,
+         #       'ld_off_{0}': cb.cmd_line_detection_off,
+          #  },
+       # },
+      #  CmdSectionType.intrusion_detection.value: {
+       #     'commands': {
+       #         'intr_on_{0}': cb.cmd_intrusion_detection_on,
+       #         'intr_off_{0}': cb.cmd_intrusion_detection_off,
+       #     },
+       # },
+       # CmdSectionType.alert_service.value: {
+       #     'commands': {
+       #         'alert_on_{0}': cb.cmd_alert_on,
+       #         'alert_off_{0}': cb.cmd_alert_off,
+       #     },
+       # },
+       # CmdSectionType.stream_youtube.value: {
+       #     'commands': {
+       #         'yt_on_{0}': cb.cmd_stream_yt_on,
+       #         'yt_off_{0}': cb.cmd_stream_yt_off,
+        #    },
+       # },
+       # CmdSectionType.stream_telegram.value: {
+        #    'commands': {
+        #        'tg_on_{0}': cb.cmd_stream_tg_on,
+        #        'tg_off_{0}': cb.cmd_stream_tg_off,
+        #    },
+       # },
+       # CmdSectionType.stream_icecast.value: {
+       #     'commands': {
+       #         'icecast_on_{0}': cb.cmd_stream_icecast_on,
+       #         'icecast_off_{0}': cb.cmd_stream_icecast_off,
+       #     },
+       # },
     }
 
     global_cmds = {
